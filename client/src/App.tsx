4565ca718b09c5131760394638682ff8ea99da52
Kristin Dailey <kristin.dailey02@gmail.com>
2025-07-23 10:04:37 -0700

    Connect and test client to server connection

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,27 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [apiResponse, setApiResponse] = useState(false);
+
+  const testConnection = async () => {
+    const apiUrl = "http://localhost:8000/api/test";
+    
+    try {
+      const res = await fetch(apiUrl);
+      const data = await res.json();
+      setApiResponse(true);
+      console.log("API connection successful:", data);
+    } catch (error) {
+      setApiResponse(false);
+      console.log("Error fetching data:", error);
+    }
+  };
+
   return (
     <>
       <h1 className="font-thin">Ciela CRM</h1>
+      <button onClick={testConnection}>Test Connection</button>
     </>
   );
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,22 @@
 import express from "express";
+import cors from "cors";
+import { timeStamp } from "console";
 
 const app = express();
+const PORT = process.env.PORT || 9000;
 
-app.listen(process.env.PORT, () => {
-    console.log("Server is listening on PORT 8000...");
+// Middleware
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+app.use(express.json());
+
+app.get("/api/test", (req, res) => {
+    res.json({ message: "Backend connected successfully!", timestamp: new Date() });
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}...`);
 });
