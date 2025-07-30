f0903d8c9e2772412d1a70b9e0ea41662aadcc38
Kristin Dailey <kristin.dailey02@gmail.com>
2025-07-30 09:02:41 -0700

    Add React compiler

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
     tailwindcss(),
   ],
 })
