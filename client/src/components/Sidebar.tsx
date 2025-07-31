188e46b586d584b671d24dc102c5b11e2071420e
Kristin Dailey <kristin.dailey02@gmail.com>
2025-07-31 09:51:38 -0700

    Refactor sidebar component for responsiveness

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { IoMdSettings } from "react-icons/io";
 import { RiLogoutBoxLine } from "react-icons/ri";
 import { useAuth } from "../context/AuthContext";
 import logo from "../assets/ciela-logo.png"; 
+import mobileLogo from "../assets/ciela-text.png";
 import { Link } from 'react-router';
 
 const Sidebar = () => {
@@ -19,39 +20,40 @@ const Sidebar = () => {
     };
 
     return (
-        <div className="min-h-screen w-48 md:w-64 bg-[var(--royal-blue)] shadow-sm border-r flex flex-col">
-            <div className="flex-1 flex flex-col items-start self-center justify-center px-4 sm:px-6 lg:px-8 text-[var(--soft-lavender)] space-y-8 pt-30 text-xl">
+        <div className="min-h-screen w-20 sm:w-35 md:w-48 bg-[var(--royal-blue)] shadow-sm border-r flex flex-col">
+            <div className="flex-1 flex flex-col items-start self-center justify-center px-2 sm:px-4 md:px-6 text-[var(--soft-lavender)] space-y-4 sm:space-y-6 md:space-y-8 pt-20 sm:pt-24 md:pt-30 text-sm sm:text-base md:text-lg">
                 <Link to="/" className="flex items-center hover:text-white">
-                    <FaHouse className="mr-2"/>
-                    <span>Home</span>
+                    <FaHouse className="text-xl mr-0 sm:mr-2"/>
+                    <span className="hidden sm:block md:block">Home</span>
                 </Link>
                 <Link to="/contacts" className="flex items-center hover:text-white">
-                    <FaUserLarge className="mr-2"/>
-                    <span>Contacts</span>
+                    <FaUserLarge className="text-xl mr-0 sm:mr-2"/>
+                    <span className="hidden sm:block md:block">Contacts</span>
                 </Link>
                 <Link to="/companies" className="flex items-center hover:text-white">
-                    <BsBuildingsFill className="mr-2"/>
-                    <span>Companies</span>
+                    <BsBuildingsFill className="text-xl mr-0 sm:mr-2"/>
+                    <span className="hidden sm:block md:block">Companies</span>
                 </Link>
                 <Link to="/applications" className="flex items-center hover:text-white">
-                    <GrDocumentText className="mr-2"/>
-                    <span>Applications</span>
+                    <GrDocumentText className="text-xl mr-0 sm:mr-2"/>
+                    <span className="hidden sm:block md:block">Applications</span>
                 </Link>
                 <Link to="/settings" className="flex items-center hover:text-white">
-                    <IoMdSettings className="mr-2"/>
-                    <span>Settings</span>
+                    <IoMdSettings className="text-xl mr-0 sm:mr-2"/>
+                    <span className="hidden sm:block md:block">Settings</span>
                 </Link>
                 <button 
                     onClick={handleLogout}
                     className="flex items-center pt-7 hover:text-white"
                 >
-                    <RiLogoutBoxLine className="mr-2"/>
-                    <span>Logout</span>
+                    <RiLogoutBoxLine className="text-xl mr-0 sm:mr-2"/>
+                    <span className="hidden sm:block md:block">Logout</span>
                 </button>
             </div>
 
             <Link to="/">
-                <img src={logo} alt="Ciela logo" className="self-end"></img>
+                <img src={mobileLogo} alt="Ciela logo" className="self-end sm:hidden"></img>
+                <img src={logo} alt="Ciela logo" className="self-end hidden sm:block"></img>
             </Link>
         </div>
     );
