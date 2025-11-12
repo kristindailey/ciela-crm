343fc781e94b4db413b0d619e84f727e1b648ec0
Kristin Dailey <kristin.dailey02@gmail.com>
2025-11-12 10:26:51 -0800

    Create InteractionCard component

---
+++ client/src/components/InteractionCard.tsx
@@ -0,0 +1,15 @@
+import type { Interaction } from "../types/interaction";
+
+interface InteractionCardProps {
+	interaction: Interaction;
+}
+
+const InteractionCard = ({ interaction }: InteractionCardProps) => {
+    return (
+        <div className="bg-gray-50 rounded-lg p-3 mb-3">
+
+		</div>
+    );
+};
+
+export default InteractionCard;
