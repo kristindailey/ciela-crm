0d10559af5238dcf0e69a051bfed887efcb524d8
Kristin Dailey <kristin.dailey02@gmail.com>
2025-12-05 15:35:11 -0800

    Create initial return statement for List component

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -16,7 +16,17 @@ interface ListProps {
 
 const List = ({ title, items, maxItems, placeholder, onAdd, onUpdate, onDelete, onClearAll }: ListProps) => {
 	return (
-		<div>List</div>
+		<div className="flex flex-col">
+			<div className="flex justify-between items-center mb-4 font-inter">
+				<h2 className="text-xl font-semibold">{title}</h2>
+				<button
+					onClick={() => {}}
+					className="text-sm text-red-600 hover:text-red-700"
+				>
+					Clear All
+				</button>
+			</div>
+		</div>
 	);
 };
 
