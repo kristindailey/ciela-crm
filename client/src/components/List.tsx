9f93881c7de1a4c6cf37251ecb1bb28c24af0954
Kristin Dailey <kristin.dailey02@gmail.com>
2025-12-05 19:06:40 -0800

    Finalize List component

--- client/src/components/List.tsx
+++ client/src/components/List.tsx
@@ -1,3 +1,5 @@
+import ListItem from "./ListItem";
+
 interface Item {
 	id?: string;
 	text: string;
@@ -13,16 +15,41 @@ interface ListProps {
 }
 
 const List = ({ title, items, maxItems, onUpdate, onDelete, onClearAll }: ListProps) => {
+	const displayItems = [...items];
+
+	if (maxItems) {
+		while (displayItems.length < maxItems) {
+			displayItems.push({ text: "" });
+		}
+	} else {
+		displayItems.push({ text: "" });
+	}
+
+
 	return (
-		<div className="flex flex-col">
+		<div className="flex flex-col bg-white border border-gray-300 rounded-lg p-4">
 			<div className="flex justify-between items-center mb-4 font-inter">
 				<h2 className="text-xl font-semibold">{title}</h2>
+
 				<button
-					onClick={() => {}}
+					onClick={onClearAll}
 					className="text-sm text-red-600 hover:text-red-700"
 				>
 					Clear All
 				</button>
+			</div>
+
+			<div>
+				{displayItems.map((item, index) => (
+					<ListItem
+						key={item.id || `empty-${index}`}
+						id={item.id}
+						value={item.text}
+						placeholder={index === items.length ? "Add new item..." : undefined}
+						onUpdate={onUpdate}
+						onDelete={onDelete}
+					/>
+				))}
 			</div>
 		</div>
 	);
