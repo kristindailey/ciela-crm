26663b5defd37b67be9b907218782aef2fe49ecd
Kristin Dailey <kristin.dailey02@gmail.com>
2025-12-10 09:31:50 -0800

    Fix Sidebar height on Dashboard page

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -3,9 +3,9 @@ import Sidebar from "./Sidebar";
 
 const Layout = () => {
 	return (
-		<div className="bg-gray-50 flex">
+		<div className="bg-gray-50 flex min-h-screen">
 			<Sidebar />
-			<div className="flex-1">
+			<div className="flex-1 ml-20 sm:ml-35 md:ml-48">
 				<Outlet />
 			</div>
 		</div>
