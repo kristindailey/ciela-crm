221e1b8ed9382850c270f2028994f0f798f9ab98
Kristin Dailey <159469428+kristindailey@users.noreply.github.com>
2026-02-15 12:00:00 -0800

    feat(landing page): update ThemeToggle to be flexible for landing page

--- client/src/components/ThemeToggle.tsx
+++ client/src/components/ThemeToggle.tsx
@@ -1,7 +1,12 @@
 import { Sun, Moon } from "lucide-react";
 import { useTheme } from "../context/ThemeContext";
 
-const ThemeToggle = () => {
+interface ThemeToggleProps {
+	className?: string;
+	showLabel?: boolean;
+}
+
+const ThemeToggle = ({ className = "flex items-center pt-7 text-sidebar-text hover:text-white", showLabel = true}: ThemeToggleProps) => {
 	const { theme, toggleTheme } = useTheme();
 	const isDark = theme === "dark";
 
@@ -9,10 +14,13 @@ const ThemeToggle = () => {
 		<button
 			onClick={toggleTheme}
 			aria-label={isDark ? "Switch to light mode" : "Switch to dark mode"}
-			className="flex items-center pt-7 text-sidebar-text hover:text-white"
+			className={className}
 		>
 			{isDark ? <Sun className="text-xl mr-0 sm:mr-2" /> : <Moon className="text-xl mr-0 sm:mr-2" />}
-			<span className="hidden sm:block md:block">{isDark ? "Light" : "Dark"}</span>
+			
+			{showLabel && (
+				<span className="hidden sm:block md:block">{isDark ? "Light" : "Dark"}</span>
+			)}
 		</button>
 	);
 };
